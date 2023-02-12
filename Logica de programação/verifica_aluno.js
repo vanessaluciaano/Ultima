6a33f0e3dd2bbd09f4039e41b3eaa36d842b9390
vanessaluciaano <vanessa.lucianav@gmail.com>
2023-02-12 13:03:10 -0300

    Exercicio Semana 1 Modulo 3

---
+++ Logica de programação/verifica_aluno.js
@@ -0,0 +1,15 @@
+// Verifica se o aluno esta aprovado ou reprovado
+
+let nota1 = 6
+let nota2 = 8
+let media 
+
+media = ((nota1 + nota2) / 2)
+
+console.log("Média:" + media)
+
+if( media > 6){
+    console.log("Aprovado")
+} else{
+    console.log("Reprovado")
+}
