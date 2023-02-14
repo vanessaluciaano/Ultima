eba5460aadd97c8399899eb1b842dda2bfbac71a
vanessaluciaano <vanessa.lucianav@gmail.com>
2023-02-14 20:56:06 -0300

    Insere nota direto no prompt

--- Logica de programação/insere_notas.js
+++ Logica de programação/insere_notas.js
@@ -1,11 +1,14 @@
 // Verifica se o aluno esta aprovado, em recuperação ou reprovado
 
-let nota1 = 60
-let nota2 = 5
+let nota1 = parseFloat(window.prompt('Digite a primeira nota:'))
+let nota2 = parseFloat(window.prompt('Digite a segunda nota:'))
+
 let media 
 
 media = ((nota1 + nota2) / 2)
 
+console.log('Primeira nota: '+ nota1);
+console.log('Segunda nota: '+nota2);
 console.log("Média: " + media)
 
 if( media >=7 && media <=10 ){
